cdbea9590b03e0fb6339bd12fa37c7dbdfd4ca4d
Burak Ozyurt <bozyurt1@gmail.com>
2016-09-09 14:39:54 -0700

    changes

--- dependencies/install_bnlp_2mvn.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mvn install:install-file -Dfile=$PWD/lib/bnlpkit-0.5.10.jar -DgroupId=bnlp -DartifactId=bnlpkit -Dversion=0.5.10 -Dpackaging=jar
--- dependencies/install_bnlp_model2mvn.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn install:install-file -Dfile=$PWD/lib/bnlpkit-models-0.6.jar -DgroupId=bnlp -DartifactId=bnlpkit-models -Dversion=0.6 -Dpackaging=jar
-
